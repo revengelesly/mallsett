aa6abd4ead8e60a7c3c9c9ef7e2c5b58e80c433f
Lesly Revenge <leslyrevenge@gmail.com>
2018-05-25 14:32:32 -0400

    Finished Product View

--- ExampleWorking/auth/client/README.md
+++
@@ -1,3 +0,0 @@
-# plugg me beta `Version 0.0.1`
-
-### Order Anything, Not Just Food.
--- ExampleWorking/src/store/epics/index.js
+++
@@ -1,9 +0,0 @@
-import { combineEpics } from 'redux-observable';
-import AuthEpic from './authEpic';
-
-const rootEpic = combineEpics(
-    AuthEpic.signupEpic,
-    AuthEpic.signinEpic,
-);
-
-export default rootEpic;
