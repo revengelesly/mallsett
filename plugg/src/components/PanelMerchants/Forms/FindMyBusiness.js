1330812f25362f6dc65435bdfe2d7f3f35f59e14
Hoa.Dang <Hoa.Dangn@coxautoinc.com>
2018-09-06 22:01:45 +0700

    fixed map issue

--- plugg/src/components/PanelMerchants/Forms/FindMyBusiness.js
+++ plugg/src/components/PanelMerchants/Forms/FindMyBusiness.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import { Row, Col } from 'antd';
 import Input, { InputGroup } from '../../../components/uielements/input';
-import Button from '../../../components/uielements/button';
 import ContentHolder from '../../../components/utility/contentHolder';
 
 import MerchantItemCard from './MerchantItemCard';
@@ -29,7 +28,7 @@ class CreateAddress extends Component {
         <InputGroup size="large" style={{ marginBottom: '15px' }}>
         <MerchantItemCard />
         </InputGroup>
-    
+
       </ContentHolder>
   </Col>
 </Row>
